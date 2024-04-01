447a66c1192c11eec7702fd279ee36b1d182b9ae
Mehran <mehranxsalehi@gmail.com>
2024-04-01 12:06:17 +0330

    Update uuid-Generator.py

--- uuid-Generator.py
+++ uuid-Generator.py
@@ -1,7 +1,6 @@
 import uuid
-
 unique_id = uuid.uuid4()
-print(unique_id)
-
 unique_id_str = unique_id.hex
+print("UUID:",unique_id)
 print("UUID HEX:", (unique_id_str))
+print("UUID FOR Identifier:", "com.apple.cellular."+(unique_id_str))
